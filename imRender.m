dde06eed2e04858337fe2f8885847461bf040848
skgironwolf <skgironwolf@users.noreply.github.com>
2017-06-14 15:47:34 -0400

    updated

--- imRender.m
+++ imRender.m
@@ -17,8 +17,13 @@ for i=1:numel(scanIDs)
     radar = rsl2mat(fileName, roo.station); 
     rmax = 150000;
     dim = 600;
+
+    %reflectivity at first elevation 
     sweep1 = radar.dz.sweeps(1);
-    sweep3 = radar.dz.sweeps(3);
+    %radial velocity at first elevation 
+    sweep3 = radar.vr.sweeps(1);
+
+    %reflectivity at second elevation 
     if strcmp(radar.station,'KDOX')
         sweep2 = radar.dz.sweeps(2);
     else
@@ -28,17 +33,22 @@ for i=1:numel(scanIDs)
     channel2 = sweep2cart(sweep2, rmax, dim);
     channel3 = sweep2cart(sweep3, rmax, dim);
     
+    %scale values to between 0 and 1 
     channel1 = (channel1 - min(channel1(:)))./(max(channel1(:)-min(channel1(:))));
     channel1(isnan(channel1)) = 0;
     channel2 = (channel2 - min(channel2(:)))./(max(channel2(:)-min(channel2(:))));
     channel2(isnan(channel2)) = 0;
     channel3 = (channel3 - min(channel3(:)))./(max(channel3(:)-min(channel3(:))));
     channel3(isnan(channel3)) = 0;
+
+    %render and save image
     image = zeros(600,600,3);
     image(:,:,1) = channel1;
     image(:,:,2) = channel2;
     image(:,:,3) = channel3;
     imwrite(image,strcat(int2str(scanIDs(i)),'.jpg'));
+
+    %This line is probably not needed if using AWS 
     save(strcat(int2str(scanIDs(i)),'.mat'),'channel1','channel2','channel3')
 end
 toc
